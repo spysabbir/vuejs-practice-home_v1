663da3045eef9f6de940877fff317704c48deb7d
spysabbir <sabbirahammedsovon@gmail.com>
2023-07-09 19:23:59 +0600

    Computed property vs Method vs Watcher

---
+++ Part-17/app.js
@@ -0,0 +1,31 @@
+
+const { createApp } = Vue
+
+createApp({
+  data() {
+    return {
+      firstName: '',
+      lastName: '',
+      fullName: '',
+    }
+  },
+  computed: {
+    // fullName(){
+    //   return this.firstName + " " + this.lastName;
+    // },
+  },
+  methods: {
+    // fullName(){
+    //   return this.firstName + " " + this.lastName;
+    // },
+  },
+  watch: {
+    firstName(newValue){
+      this.fullName = newValue + " " + this.lastName;
+    },
+    lastName(newValue){
+      this.fullName = this.firstName + " " + newValue;
+    }
+  }
+
+}).mount('#app')
