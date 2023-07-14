ea9ebe7be341354f5d4d4e68eaabf3a5846ae359
spysabbir <sabbirahammedsovon@gmail.com>
2023-07-14 23:24:32 +0600

    Introduction to Components

---
+++ Part-23/app.js
@@ -0,0 +1,21 @@
+
+// const { createApp } = Vue
+
+// createApp({
+//   data() {
+//     return {
+   
+//     }
+//   },
+//   methods: {
+   
+//   }
+// }).mount('#app')
+
+
+function handelKeyup(el){
+  console.log(el.value);
+}
+function handelClick(){
+  console.log('Button Clicked');
+}
