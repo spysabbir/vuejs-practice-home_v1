de98a40a9e93939a2b80dbf3070ec4e5f7e8f153
spysabbir <sabbirahammedsovon@gmail.com>
2023-07-18 22:06:40 +0600

    Adding style to components

---
+++ Part-30/src/Test.vue
@@ -0,0 +1,9 @@
+<script setup>
+import ContactDetails from './components/ContactDetails.vue'
+</script>
+
+<template>
+  <div>
+      <ContactDetails />
+  </div>
+</template>
---
+++ Part-30/src/components/ContactDetails.vue
@@ -0,0 +1,31 @@
+<template>
+<div class="contact-details">
+    <h1 class="contact-title">Contact Details</h1>
+    <p><strong>Name:</strong>Sabbir Ahammed</p>
+    <p><strong>Phone:</strong>01517 805999</p>
+    <p><strong>Address:</strong>Jessore, BD</p>
+    <p><strong>Email:</strong>spysabbir@spysabbir.com</p>
+    <p><strong>Web:</strong>spysabbir.com</p>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "Test Vue",
+        }
+    }  
+}
+</script>
+
+<style scoped>
+.contact-details {
+    border: 1px solid red;
+    padding: 15px;
+}
+.contact-title{
+    color: blue;
+    font-size: 26px;
+}
+</style>
