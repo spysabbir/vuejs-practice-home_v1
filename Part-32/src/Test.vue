9dbf70d9e925da3efa07391f17442548c57e0514
spysabbir <sabbirahammedsovon@gmail.com>
2023-07-18 22:23:22 +0600

    Component custom properties

---
+++ Part-32/src/Test.vue
@@ -0,0 +1,11 @@
+<script setup>
+import ContactDetails from './components/ContactDetails.vue'
+</script>
+
+<template>
+  <div>
+      <ContactDetails name="Sabbir Ahammed" email="spysabbir@spysabbir.com" web-address="spysabbir.com"/>
+      <br>
+      <ContactDetails name="Sovon Khan" email="spysovon@spysovon.com" web-address="spysovon.com"/>
+  </div>
+</template>
---
+++ Part-32/src/components/ContactDetails.vue
@@ -0,0 +1,30 @@
+<template>
+<div class="contact-details">
+    <h1 class="contact-title">Contact Details</h1>
+    <p><strong>Name: </strong>{{ name }}</p>
+    <p><strong>Email: </strong>{{ email }}</p>
+    <p><strong>Web: </strong>{{ webAddress }}</p>
+</div>
+</template>
+
+<script>
+export default {
+    props: ['name', 'email', 'webAddress'],
+    data() {
+        return {
+            message: "Test Vue",
+        }
+    }  
+}
+</script>
+
+<style scoped>
+.contact-details {
+    border: 1px solid red;
+    padding: 15px;
+}
+.contact-title{
+    color: blue;
+    font-size: 26px;
+}
+</style>
