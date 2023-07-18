18d0e3111be8d4b0fe49ad12c00b5f183b1de2d8
spysabbir <sabbirahammedsovon@gmail.com>
2023-07-18 21:09:34 +0600

    Recreating the component with CLI

---
+++ Part-29/src/Test.vue
@@ -0,0 +1,17 @@
+<script setup>
+// export default {
+//   data() {
+//     return {
+//         message: "Test Vue",
+//     }
+//   }  
+// }
+import ContactDetails from './components/ContactDetails.vue'
+</script>
+
+<template>
+  <div>
+      <!-- <h1>{{ message }}</h1> -->
+      <ContactDetails />
+  </div>
+  </template>
---
+++ Part-29/src/components/ContactDetails.vue
@@ -0,0 +1,20 @@
+<template>
+<div>
+    <h1>Contact Details</h1>
+    <p><strong>Name:</strong>Sabbir Ahammed</p>
+    <p><strong>Phone:</strong>01517 805999</p>
+    <p><strong>Address:</strong>Jessore, BD</p>
+    <p><strong>Email:</strong>spysabbir@spysabbir.com</p>
+    <p><strong>Web:</strong>spysabbir.com</p>
+</div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            message: "Test Vue",
+        }
+    }  
+}
+</script>
