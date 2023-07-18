8fa29a728673b1cf9055de6273239efdf40b1ffa
spysabbir <sabbirahammedsovon@gmail.com>
2023-07-18 23:15:57 +0600

    Props Validation

---
+++ Part-33/src/Test.vue
@@ -0,0 +1,33 @@
+<script>
+import ContactDetails from './components/ContactDetails.vue';
+
+export default {
+  components: {
+    ContactDetails
+  },
+  data() {
+    return {
+      contacts: [
+        { name: "Sabbir Ahammed", email: "spysabbir@spysabbir.com", web: "spysabbir.com" },
+        { name: "Sabbir Ahammed 2", email: "spysabbir2@spysabbir.com", web: "spysabbir2.com" },
+        { name: "Sabbir Ahammed 3", email: "spysabbir3@spysabbir.com", web: "spysabbir3.com" },
+      ]
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <ContactDetails name="Sabbir Ahammed" email="spysabbir@spysabbir.com" web-address="spysabbir.com" />
+    <ContactDetails name="Sovon Khan" email="spysovon@spysovon.com" />
+
+    <ContactDetails
+      v-for="contact in contacts"
+      :key="contact.name"
+      :name="contact.name"
+      :email="contact.email"
+      :web-address="contact.web"
+    />
+  </div>
+</template>
