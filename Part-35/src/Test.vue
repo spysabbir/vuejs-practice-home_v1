800e2b14afd39429d425a6d8b0b49cd584b546c9
spysabbir <sabbirahammedsovon@gmail.com>
2023-07-28 23:34:55 +0600

    Named Slots

---
+++ Part-35/src/Test.vue
@@ -0,0 +1,43 @@
+<script>
+import TheCard from './TheCard.vue';
+
+export default {
+  components: {
+    TheCard,
+},
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <TheCard cardTitle="About Me">
+      <template v-slot:default>
+        <div class="contact-details">
+          <h1 class="contact-title">Contact Details</h1>
+          <p><strong>Name: </strong>Sabbir Ahammed</p>
+        </div>
+      </template>
+      <template v-slot:footer>
+          <p><strong>Web: </strong>www.spysabbir.com</p>
+      </template>
+    </TheCard>
+
+    <TheCard cardTitle="Apple iPhone 14 Pro Max">
+      <template #default>
+        <img src="https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-14-pro-max-.jpg" alt="">
+      </template>
+      <template #footer>
+          <a href="http://www.spysabbir.com">Buy Now</a>
+      </template>
+    </TheCard>
+
+    <TheCard cardTitle="Default Check">
+
+    </TheCard>
+  </div>
+</template>
---
+++ Part-35/src/TheCard.vue
@@ -0,0 +1,46 @@
+<script>
+export default {
+  props: ["cardTitle"]
+}
+</script>
+
+<template>
+  <div class="the-card">
+    <div class="the-card__title">
+        {{ cardTitle }}
+    </div>
+    <div class="the-card__body">
+        <slot>
+          Default card slot text.
+        </slot>
+    </div>
+    <div class="the-card__footer">
+      <slot name="footer">
+        Default footer
+      </slot>
+    </div>
+  </div>
+</template>
+
+<style>
+.the-card{
+    width: 200px;
+    border: 1px solid red;
+    min-height: 200px;;
+    margin: 22px auto;
+}
+
+.the-card__title{
+    background-color: aqua;
+    padding: 5px 11px;
+    color: #fff;
+}
+.the-card__body{
+    padding: 11px;
+}
+.the-card__footer{
+    background-color: blueviolet;
+    padding: 5px 11px;
+    text-align: center;
+}
+</style>
