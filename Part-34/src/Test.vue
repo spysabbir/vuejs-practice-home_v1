43a6e51a2ad312cd298d66f7447812d11a0589f7
spysabbir <sabbirahammedsovon@gmail.com>
2023-07-28 23:21:57 +0600

    Component Slot

---
+++ Part-34/src/Test.vue
@@ -0,0 +1,35 @@
+<script>
+import TheCard from './TheCard.vue';
+
+export default {
+  components: {
+    TheCard,
+},
+  data() {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <TheCard cardTitle="About Me">
+      <div class="contact-details">
+        <h1 class="contact-title">Contact Details</h1>
+        <p><strong>Name: </strong>Sabbir Ahammed</p>
+      </div>
+    </TheCard>
+    <TheCard cardTitle="Apple iPhone 14 Pro Max">
+      <img src="https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-14-pro-max-.jpg" alt="">
+    </TheCard>
+    <TheCard cardTitle="Services">
+      OS:	iOS 16, upgradable to iOS 16.5, planned upgrade to iOS 17
+      Chipset:	Apple A16 Bionic (4 nm)
+      CPU:	Hexa-core (2x3.46 GHz Everest + 4x2.02 GHz Sawtooth)
+      GPU:	Apple GPU (5-core graphics)
+    </TheCard>
+    <TheCard cardTitle="Default Check"></TheCard>
+  </div>
+</template>
---
+++ Part-34/src/TheCard.vue
@@ -0,0 +1,39 @@
+<script>
+export default {
+  props: ["cardTitle"]
+}
+</script>
+
+<template>
+  <div class="the-card">
+    <div class="the-card__title">
+        {{ cardTitle }}
+    </div>
+    <div class="the-card__body">
+        <slot>
+          Default card slot text.
+        </slot>
+    </div>
+  </div>
+</template>
+
+<style>
+.the-card{
+    width: 200px;
+    border: 1px solid red;
+    min-height: 200px;;
+    margin: 22px auto;
+}
+
+.the-card__title{
+    background-color: aqua;
+    padding: 5px 11px;
+    color: #fff;
+}
+.the-card__body{
+    padding: 11px;
+}
+.the-card__body img{
+    height: 111px;
+}
+</style>
