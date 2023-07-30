70b7e5d90671b94a3fe6a1262bca105407c1af9c
spysabbir <sabbirahammedsovon@gmail.com>
2023-07-30 23:56:18 +0600

    component communications

---
+++ Part-38/src/ProductCard.vue
@@ -0,0 +1,63 @@
+<script>
+export default {
+  mounted() {
+  },
+  emits: {
+    'buy-now-btn-click': function(data){
+      if(!data) {
+        console.log('data not found');
+        return false;
+      }
+      return true;
+    }
+  },
+  props: {"product": {type: Object, default: () => ({})}},
+  methods: {
+    handleBuyNowClick() {
+      this.$emit("buy-now-btn-click", this.product)
+    },
+    handleaddToCartClick() {
+      this.$emit("add-to-cart-btn-click", this.product)
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="product-card">
+    <div class="product-card__title">
+      {{ product.name }}
+    </div>
+    <div class="product-card__body">
+      <img :src="product.thumbnail" alt="">
+      <span>{{ product.price }}</span>
+    </div>
+    <div class="product-card__footer">
+      <button @click="handleBuyNowClick">Buy Now</button>
+      <button @click="handleaddToCartClick">Add to Cart</button>
+    </div>
+  </div>
+</template>
+
+<style>
+.product-card{
+    width: 200px;
+    border: 1px solid red;
+    min-height: 200px;;
+    margin: 22px auto;
+}
+
+.product-card__title{
+    background-color: aqua;
+    padding: 5px 11px;
+    color: #fff;
+}
+.product-card__body{
+    padding: 11px;
+}
+.product-card__footer{
+    background-color: blueviolet;
+    padding: 5px 11px;
+    text-align: center;
+}
+</style>
---
+++ Part-38/src/TheCard.vue
@@ -0,0 +1,49 @@
+<script>
+export default {
+  mounted() {
+    console.log(this.$slots.footer)
+  },
+  props: ["cardTitle"]
+}
+</script>
+
+<template>
+  <div class="the-card">
+    <div class="the-card__title">
+        {{ cardTitle }}
+    </div>
+    <div class="the-card__body">
+        <slot>
+          Default card slot text.
+        </slot>
+    </div>
+    <div class="the-card__footer" v-if="$slots.footer">
+      <slot name="footer">
+        
+      </slot>
+    </div>
+  </div>
+</template>
+
+<style>
+.the-card{
+    width: 200px;
+    border: 1px solid red;
+    min-height: 200px;;
+    margin: 22px auto;
+}
+
+.the-card__title{
+    background-color: aqua;
+    padding: 5px 11px;
+    color: #fff;
+}
+.the-card__body{
+    padding: 11px;
+}
+.the-card__footer{
+    background-color: blueviolet;
+    padding: 5px 11px;
+    text-align: center;
+}
+</style>
