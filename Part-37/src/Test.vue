5f26d3818c1ce252161d3e866ff402078b878eb7
spysabbir <sabbirahammedsovon@gmail.com>
2023-07-30 21:52:44 +0600

    component custom event

---
+++ Part-37/src/Test.vue
@@ -0,0 +1,45 @@
+<script>
+import ProductCard from './ProductCard.vue';
+
+export default {
+  components: {
+    ProductCard,
+},
+  data() {
+    return {
+      // product:{
+      //   name: "Apple iPhone 14 Pro Max",
+      //   thumbnail: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-14-pro-max-.jpg",
+      //   price: "$1600"
+      // }
+      products:[
+      {
+        name: "Apple iPhone 12 Pro",
+        thumbnail: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-14-pro-max-.jpg",
+        price: "$1600"
+      },
+      {
+        name: "Apple iPhone 14 Pro Max",
+        thumbnail: "https://fdn2.gsmarena.com/vv/bigpic/apple-iphone-14-pro-max-.jpg",
+        price: "$1800"
+      },
+      ]
+    }
+  },
+  methods: {
+    BuyNowBtn(product) {
+      console.log(product)
+    },
+    AddToCart(product) {
+      console.log(product)
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <!-- <product-card :product="product" @buy-now-btn-click="BuyNowBtn"> </product-card> -->
+    <product-card v-for="product in products" :key="product.name" :product="product" @buy-now-btn-click="BuyNowBtn" @add-to-cart-btn-click="AddToCart"> </product-card>
+  </div>
+</template>
---
+++ Part-37/src/components/ContactDetails.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="contact-details">
+      <h1 class="contact-title">Contact Details</h1>
+      <p><strong>Name: </strong>{{ name }}</p>
+      <p><strong>Email: </strong>{{ email }}</p>
+      <p><strong>Web: </strong>{{ webAddress }}</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: {
+      name: { type: String },
+      email: { type: String, required: true },
+      webAddress: { type: String, required: false, default: 'www.default.com' },
+    },
+  }
+  </script>
+  
+  <style scoped>
+  .contact-details {
+    border: 1px solid red;
+    padding: 15px;
+    margin-bottom: 5px;
+  }
+  .contact-title {
+    color: blue;
+    font-size: 26px;
+  }
+  </style>
+  
