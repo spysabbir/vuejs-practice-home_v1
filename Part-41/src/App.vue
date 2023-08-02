58958f8c7828f5878d4a8daa82ece00f8549c03c
spysabbir <sabbirahammedsovon@gmail.com>
2023-08-03 00:15:49 +0600

    Adding v-model support

---
+++ Part-41/src/App.vue
@@ -0,0 +1,24 @@
+<script>
+import TheRating from './TheRating.vue';
+
+export default {
+  components: {
+    TheRating,
+},
+  data() {
+    return {
+      myRating: 3,
+    }
+  },
+  methods: {
+   
+  }
+}
+</script>
+
+<template>
+  <div>
+    <p>My Rating {{ myRating }}</p>
+    <the-rating v-model="myRating"></the-rating>
+  </div>
+</template>
