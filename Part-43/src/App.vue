09e4b173e60ebfe72e9dc6f9f69cb007c060e82d
spysabbir <sabbirahammedsovon@gmail.com>
2023-08-25 23:38:55 +0600

    Non-Prop Attributes

---
+++ Part-43/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <!-- <TheComment v-model="myComment" btnText="Submit" class="bar" placeholder="Enter Comment"></TheComment> -->
+    <TheComment v-model="myComment" btnText="Submit" placeholder="Enter Comment" @click="handelClick"></TheComment>
+
+    <br><hr><br>
+
+    <p>Comment: {{ myComment }}</p>
+  </div>
+</template>
+
+<script>
+import TheComment from './components/TheComment.vue';
+
+export default {
+  components: {
+    TheComment,
+},
+  data() {
+    return {
+      myComment: "",
+    }
+  },
+  methods: {
+    handelClick(){
+      alert('Div Click')
+    }
+  }
+}
+</script>
+
+<style>
+.container{
+  padding: 22px;
+}
+</style>
---
+++ Part-43/src/components/TheComment.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="the-comment">
+        <!-- <textarea :value="modelValue" @input="handelInput"></textarea> -->
+        <textarea :value="modelValue" @input="handelInput" v-bind="$attrs"></textarea>
+        <button>{{ btnText }}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        modelValue: {
+            type: String,
+            default: ""
+        },
+        btnText: {
+            type: String,
+            default: 'Ok'   
+        }
+    },
+    // inheritAttrs: false,
+    methods: {
+        handelInput(e){
+            this.$emit('update:modelValue', e.target.value);
+        }
+    }
+}
+</script>
+
+<style>
+.the-comment{
+    width: 288px;
+    border: 1px solid grey;
+    padding: 22px;
+    border-radius: 5px;
+    background-color: azure;
+    color: #313131;
+    min-height: 111px;
+}
+</style>
