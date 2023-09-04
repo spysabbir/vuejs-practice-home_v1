72ec503948d3273cbbd4d16321af84877710e64b
spysabbir <sabbirahammedsovon@gmail.com>
2023-09-04 23:58:34 +0600

    Working with references -

---
+++ Part-49/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <input type="text" placeholder="Enter name" v-model="name" ref="nameInput">
+
+    <br>
+
+    <the-comment v-model="comment" placeholder="Enter Comment ..." ref="commentInput">
+
+    </the-comment>
+  </div>
+</template>
+
+<script>
+import TheComment from './components/TheComment.vue';
+
+export default {
+  components: {
+    TheComment,
+  },
+  data() {
+    return {
+      name: "",
+      comment: "",
+    }
+  },
+  mounted() {
+    console.log(this.$refs.nameInput);
+    console.log(this.$refs.commentInput);
+  },
+}
+</script>
+
+<style>
+</style>
---
+++ Part-49/src/components/TheComment.vue
@@ -0,0 +1,39 @@
+<template>
+    <h2>Right your comment below</h2>
+    <div class="the-comment">
+        <textarea :value="modelValue" @input="handelInput" v-bind="$attrs"></textarea>
+        <button @click="$emit('continueClick')">{{ btnText }}</button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        modelValue: {
+            type: String,
+            default: ""
+        },
+        btnText: {
+            type: String,
+            default: 'Ok'   
+        }
+    },
+    methods: {
+        handelInput(e){
+            this.$emit('update:modelValue', e.target.value);
+        }
+    }
+}
+</script>
+
+<style>
+.the-comment{
+    width: 288px;
+    border: 1px solid grey;
+    padding: 22px;
+    border-radius: 5px;
+    background-color: azure;
+    color: #313131;
+    min-height: 111px;
+}
+</style>
