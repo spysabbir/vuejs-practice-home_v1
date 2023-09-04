1461c57b56119ef7928eef419817bc82c1f20a0a
spysabbir <sabbirahammedsovon@gmail.com>
2023-09-04 23:28:01 +0600

    Dynamic and async components

---
+++ Part-47/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div class="container">
+   <div class="tab">
+    <div class="tab__menu">
+      <div class="tab_menu-item" 
+          v-for="tab in tabs" :key="tab" 
+          :class="{'tab_menu-item-active' : activeTab == tab}" 
+          @click="activeTab = tab">
+        {{ tab }}
+      </div>
+    </div>
+    <div class="tab__content">
+      <!-- <Login v-if="activeTab === 'Login'"></Login>
+      <Register v-if="activeTab === 'Register'"></Register> -->
+
+      <KeepAlive>
+        <component :is="activeTab">
+
+        </component>
+      </KeepAlive>
+    </div>
+   </div>
+  </div>
+</template>
+
+<script>
+import Login from './components/Login.vue';
+import Register from './components/Register.vue';
+
+export default {
+  components: {
+    Login,
+    Register,
+},
+  data() {
+    return {
+      tabs: ["Login", "Register"],
+      activeTab: "Login",
+    }
+  },
+  methods: {
+  
+  },
+}
+</script>
+
+<style>
+.tab__menu{
+  display: flex;
+
+}
+.tab_menu-item{
+  padding: 5px;
+  background-color: beige;
+  margin: 2px;
+  cursor: pointer;
+}
+.tab_menu-item-active{
+  background-color: blueviolet;
+  color: aliceblue;
+
+}
+
+.tab__content{
+  height: 250px;
+  width: 250px;
+  background-color: chocolate;
+}
+</style>
---
+++ Part-47/src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Login</h1>
+    <form>
+        <div>
+          <label>Email</label>
+          <input type="text" placeholder="Email">
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
