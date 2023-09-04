e3eb2ae0d290003b128120f7031a2da220f243cf
spysabbir <sabbirahammedsovon@gmail.com>
2023-09-05 00:06:21 +0600

    Official Style guide

---
+++ Part-50/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="container">
+
+    <h1>Official Style guide</h1>
+    <a href="https://vuejs.org/style-guide/">Visit</a>
+
+    <br><hr>
+    
+    <input type="text" placeholder="Enter name" v-model="name" ref="nameInput">
+
+    <br>
+
+    <the-comment v-model="comment" placeholder="Enter Comment ..." ref="commentInput">
+
+    </the-comment>
+  </div>
+</template>
+
+<script>
+import TheComment from './components/TheComment.vue';
+
+export default {
+  components: {
+    TheComment,
+  },
+  data() {
+    return {
+      name: "",
+      comment: "",
+    }
+  },
+  mounted() {
+    console.log(this.$refs.nameInput);
+    console.log(this.$refs.commentInput);
+  },
+}
+</script>
+
+<style>
+</style>
