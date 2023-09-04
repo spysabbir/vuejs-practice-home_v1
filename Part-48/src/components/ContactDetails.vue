c66204cd18788795708cc92a2e2b896a5ea3fbbd
spysabbir <sabbirahammedsovon@gmail.com>
2023-09-04 23:45:26 +0600

    Dependency injection with provide-inject

---
+++ Part-48/src/components/ContactDetails.vue
@@ -0,0 +1,19 @@
+<template>
+    <h1>Contact Details</h1>
+    <div>
+        <span>Name: {{ name }}</span>
+    </div>
+    <div>
+        <span>Email: {{ email }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    inject: ["name", "email"]
+}
+</script>
+
+<style>
+
+</style>
---
+++ Part-48/src/components/Register.vue
@@ -0,0 +1,22 @@
+<template>
+    <h1>Register</h1>
+    <form>
+        <div>
+          <label>Name</label>
+          <input type="text" placeholder="Name">
+        </div>
+        <div>
+          <label>Email</label>
+          <input type="text" placeholder="Email">
+        </div>
+    </form>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
