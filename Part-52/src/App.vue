0972889147d33fce4c9eaa625c7c1088e8484197
spysabbir <sabbirahammedsovon@gmail.com>
2023-09-05 21:43:14 +0600

    Working with forms - Radio Button

---
+++ Part-52/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="container">
+    <h1>New User Register</h1>
+
+    <Label>Full Name</Label><br>
+    <input type="text" placeholder="Enter your full name" v-model="formData.fullName">
+    <hr>
+    
+    <Label>About Me</Label><br>
+    <textarea placeholder="Enter your about me" v-model="formData.aboutMe"></textarea>
+    <hr>
+
+    <Label>Gender</Label><br>
+    <label><input type="radio" name="gender" value="Male" v-model="formData.gender">Male</label>
+    <label><input type="radio" name="gender" value="Female" v-model="formData.gender">Female</label>
+    <label><input type="radio" name="gender" value="Other" v-model="formData.gender">Other</label>
+
+
+    <hr>
+    <div v-for="(value, key) in formData" :key="key">
+    <span><strong>{{ key }}:</strong> {{ value }}</span>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      formData: {
+        fullName: "",
+        aboutMe: "",
+        gender: '',
+      },
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
