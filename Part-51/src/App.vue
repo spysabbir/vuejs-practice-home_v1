f0927d6385f42899b93e3d98ff86d780d42e9a86
spysabbir <sabbirahammedsovon@gmail.com>
2023-09-05 21:31:16 +0600

    Working with forms - input text, textarea

---
+++ Part-51/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="container">
+    <h1>New User Register</h1>
+
+    <Label>Full Name</Label><br>
+    <input type="text" placeholder="Enter your full name" v-model="formData.fullName">
+    <hr>
+    <Label>About Me</Label><br>
+    <textarea placeholder="Enter your about me" v-model="formData.aboutMe"></textarea>
+    <hr>
+
+    <div v-for="(value, key) in formData" :key="key">
+    <span><strong>{{ key }}:</strong> {{ value }}</span>
+    </div>
+
+  </div>
+</template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      formData: {
+        fullName: "",
+        aboutMe: "",
+      },
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
